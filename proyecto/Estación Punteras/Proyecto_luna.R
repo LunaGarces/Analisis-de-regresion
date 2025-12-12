ad76d14e5a0be74dcac071ca7a9ccbc19eb7c392
LunaGarces <luna.garcs@uc.cl>
2025-12-12 14:00:52 -0300

    datos nuevos

--- proyecto/Estación Punteras/Proyecto_luna.R
+++ proyecto/Estación Punteras/Proyecto_luna.R
@@ -22,6 +22,7 @@ train_indices <- sample(1:nrow(data_met), size = 0.8 * nrow(data_met))
 train_data <- data_met[train_indices, ]
 test_data <- data_met[-train_indices, ]
 
+
 #####################################
 ## PCA para var con max, min, mean ##
 #####################################
@@ -31,7 +32,7 @@ test_data <- data_met[-train_indices, ]
 ######################################
 temps <- train_data %>% dplyr::select(temperatura_mean, temperatura_max, temperatura_min)
 pca_temp <- prcomp(temps, scale. = TRUE)
-train_data$temp_pca <- pca_temp$x[, 1] #Valores ALTOS de temp_pca significan FRÍO
+train_data$temp_pca <- pca_temp$x[, 1]
 train_data <- train_data %>% dplyr::select(-temperatura_mean, -temperatura_max, -temperatura_min)
 pca_temp$rotation
 
@@ -60,11 +61,14 @@ names(train_data)
 #########################
 
 modelo_inicial <- lm(MP2_5 ~ temp_pca + humidity_pca + vel_viento_pca + 
-                       precipitacion_mean + precipitacion_max,
+                       precipitacion_mean + precipitacion_max + 
+                       direccion_viento_mean + direccion_viento_max + 
+                       direccion_viento_min,
                      data = train_data)
 modelo_backward <- step(modelo_inicial, direction = "backward")
+
 summary(modelo_inicial)
-summary(modelo_backward) # lm(formula = MP10 ~ temp_pca + humidity_pca + precipitacion_mean, data = train_data)
+summary(modelo_backward) # lm(formula = MP2_5 ~ temp_pca + direccion_viento_mean + direccion_viento_max + precipitacion_max, data = train_data)
 
 
 ##############################
@@ -75,5 +79,66 @@ summary(modelo_backward) # lm(formula = MP10 ~ temp_pca + humidity_pca + precipi
 
 
 # transformacion a MP2_5
+
+##############################
+### Transformación Box-Cox ### 
+##############################
+
+# 1. Verificar si necesitamos un offset (Box-Cox no acepta ceros ni negativos)
+min_y <- min(train_data$MP2_5, na.rm = TRUE)
+offset <- 0
+if (min_y <= 0) {
+  offset <- abs(min_y) + 0.1 # Sumamos una constante pequeña si es necesario
+  cat("Se aplicó un offset de:", offset, "\n")
+}
+
+# 2. Calcular el Lambda óptimo usando el modelo seleccionado (modelo_backward)
+# Se graficará la curva de verosimilitud para que veas el pico
+bc <- boxcox(modelo_backward, plotit = TRUE, lambda = seq(-1, 1, by = 0.1))
+
+# Extraer el lambda exacto donde se maximiza la curva
+lambda_opt <- bc$x[which.max(bc$y)]
+cat("El Lambda óptimo es:", round(lambda_opt, 3), "\n")
+
+# 3. Aplicar la transformación a MP2_5 (Tanto en Train como en Test)
+# Definimos una función para transformar
+trans_boxcox <- function(y, lambda, offset = 0) {
+  y_adj <- y + offset
+  if (abs(lambda) < 0.1) { # Si lambda es casi 0, usamos Log
+    return(log(y_adj))
+  } else {                 # Si no, usamos la fórmula de potencia
+    return((y_adj^lambda - 1) / lambda)
+  }
+}
+
+# Creamos la nueva variable transformada en ambos sets
+train_data$MP2_5_bc <- trans_boxcox(train_data$MP2_5, lambda_opt, offset)
+test_data$MP2_5_bc  <- trans_boxcox(test_data$MP2_5, lambda_opt, offset)
+
+# 4. Ajustar el nuevo modelo con la respuesta transformada
+# Usamos las mismas predictoras que eligió el backward, pero cambiamos la Y
+formula_final <- formula(modelo_backward)
+# Reemplazamos la variable respuesta en la fórmula (truco de R)
+formula_bc <- update(formula_final, MP2_5_bc ~ .)
+
+modelo_final_bc <- lm(formula_bc, data = train_data)
+
+# Ver resultados
+summary(modelo_final_bc)
+
+# 5. Diagnóstico rápido: ¿Mejoraron los residuos?
+par(mfrow = c(1, 2))
+qqnorm(residuals(modelo_backward), main = "QQ-Plot: Modelo Original", pch=20); qqline(residuals(modelo_backward), col="red")
+qqnorm(residuals(modelo_final_bc), main = "QQ-Plot: Modelo Box-Cox", pch=20); qqline(residuals(modelo_final_bc), col="red")
+par(mfrow = c(1, 1))
+
+
+
+
+
+
+
+
+
 
 
